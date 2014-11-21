25668aa227324ba8e00e77b03a06bddb065d7ae3
Mark Burnett <mburnett@genome.wustl.edu>
2014-11-21 09:15:35 -0600

    add GPL3 license

--- lsf/request.py
+++ lsf/request.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2014 The Genome Institute, Washington University Medical School
+#
+# This file is part of lsf-python.
+#
+# lsf-python is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# lsf-python is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with lsf-python.  If not, see <http://www.gnu.org/licenses/>.
+
 from . import bindings
 from . import job
 from .rlimits import set_rlimits
